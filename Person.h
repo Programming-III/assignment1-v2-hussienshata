79882b5544f279c6ee4596fb72c948447d3d2245
Hussien Ahmed Shata <130321278+hussienshata@users.noreply.github.com>
2025-11-11 09:49:21 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,53 @@
-#ifndef PERSON_H
-#define PERSON_H
-
-#include <string>
+#ifndef UNTITLED_PERSON_H
+#define UNTITLED_PERSON_H
+#include <iostream>
 using namespace std;
 
-//#write Person class here
+class Person {
+    private:
+    string name;
+    int id;
 
+    public:
+    Person();
+    Person(string name, int id);
+    void setName(string name);
+    void setID(int id);
+    string getName();
+    int getID();
+    void display();
 
+};
 
+class Student: Person {
+    private:
+    int yearLevel;
+    string major;
 
+    public:
+    Student();
+    Student(int yearLevel, string major);
+    void setYearLevel(int yearLevel);
+    void setMajor(string major);
+    string getMajor();
+    int getYearLevel();
+    void display();
 
+};
 
+class Instructor: Person {
+    private:
+    string department;
+    int experienceYears;
 
+    public:
+    Instructor();
+    Instructor(string department, int experienceYears);
+    void setexperienceYears(int experienceYears);
+    void setDepartment(string department);
+    string getDepartment();
+    int getexperienceYears();
+    void display();
+};
 
-
-
-
-
-#endif
+#endif //UNTITLED_PERSON_H
